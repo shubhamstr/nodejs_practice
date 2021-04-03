82d1b4bbd5db5fade4c1906f810caafd60b5a1ce
shubhamstr <shubham.sutar@vinnovatetechnologies.com>
2021-04-03 20:51:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+// fs.mkdirSync("thapa");
+// fs.writeFileSync("thapa/bio.txt", "hello");
+// fs.appendFileSync("thapa/bio.txt", "shubham");
+// fs.readFileSync("thapa/bio.txt", "utf8");
+// fs.renameSync("thapa/bio.txt", "thapa/mybio.txt");
+// fs.unlinkSync("thapa/mybio.txt");
+// fs.rmdirSync("thapa");
+// fs.unlinkSync("bio.txt");
+// console.log("hello");
