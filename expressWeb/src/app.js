d6a779900941e3fc3dff11567e9bf9dccd52b1e9
shubhamstr <shubham.sutar@vinnovatetechnologies.com>
2021-04-27 00:03:46 +0530

    weather app created

--- expressWeb/src/app.js
+++ expressWeb/src/app.js
@@ -1,23 +1,41 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const hbs = require('hbs')
 
 const port = process.env.PORT || 8000
 
+
+console.log(path.join(__dirname, "../public"));
+const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set('view engine', 'hbs');
+app.set('views', templatePath)
+hbs.registerPartials(partialsPath)
+app.use(express.static(staticPath));
+
+
 app.get("", (req, res) => {
-    res.send("welcome my site");
+    // res.send("welcome my site");
+    res.render("index");
 });
 
 app.get("/about", (req, res) => {
-    res.send("welcome my about page");
+    // res.send("welcome my about page");
+    res.render("about");
 });
 
 app.get("/weather", (req, res) => {
-    res.send("welcome my weather page");
+    // res.send("welcome my weather page");
+    res.render("weather");
 });
 
 
 app.get("*", (req, res) => {
-    res.send("404 page");
+    // res.send("404 page");
+    res.render("404");
 });
 
 app.listen(port, () => {
