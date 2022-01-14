03772631d60939d8df77fe9aac14799153b01a0d
shubhamstr <shubhamsutar5799@gmail.com>
2022-01-15 02:01:08 +0530

    12 import export

---
+++ vdo12ImportExport1.js
@@ -0,0 +1,12 @@
+// const add = require("./vdo12ImportExport2");
+// const oper = require("./vdo12ImportExport2");
+const {add,sub} = require("./vdo12ImportExport2");
+
+
+// console.log(add(5,5));
+
+// console.log(oper.add(5,5));
+// console.log(oper.sub(5,5));
+
+console.log(add(5,5));
+console.log(sub(5,5));
