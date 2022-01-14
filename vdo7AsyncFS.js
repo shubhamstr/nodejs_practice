3c88aa63a0a152cc993a2397e8204666cdbbabd2
shubhamstr <shubhamsutar5799@gmail.com>
2022-01-15 01:43:15 +0530

    5 6 7 tuts fs async

---
+++ vdo7AsyncFS.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+fs.writeFile('vdo7.txt','hello1', (err) => {
+    console.log('created file');
+    console.log(err);
+});
+
+fs.appendFile('vdo7.txt','shubham', (err) => {
+    console.log('append file');
+    console.log(err);
+});
+
+fs.readFile('vdo7.txt', 'utf-8', (err, data) => {
+    console.log(data);
+    console.log(err);
+});
+
+// fs.rename('vdo7.txt', 'vdo7m.txt');
