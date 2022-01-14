6d182865dd2db752f6dd154b3408bb633c992024
shubhamstr <shubhamsutar5799@gmail.com>
2022-01-15 02:03:48 +0530

    12 import export update

--- vdo12ImportExport2.js
+++ vdo12ImportExport2.js
@@ -1,18 +1,18 @@
-// const add = (a,b) =>{
-//     return a+b;
-// }
-
-// module.exports = add;
-
-
 const add = (a,b) =>{
     return a+b;
 }
-const sub = (a,b) =>{
-    return a-b;
-}
 
-// module.exports.add = add;
-// module.exports.sub = sub;
+module.exports = add;
 
-module.exports = {add, sub};
+
+// const add = (a,b) =>{
+//     return a+b;
+// }
+// const sub = (a,b) =>{
+//     return a-b;
+// }
+
+// // module.exports.add = add;
+// // module.exports.sub = sub;
+
+// module.exports = {add, sub};
